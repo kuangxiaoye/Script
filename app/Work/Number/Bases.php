e3e64968ab14bac04ea9159a97b1e57231137cad
wangye <wangye@miot.cn>
2019-01-06 09:34:06 +0800

    手机靓号扫号脚本--添加返回值判断

--- app/Work/Number/Bases.php
+++ app/Work/Number/Bases.php
@@ -21,38 +21,41 @@ class Bases extends BasesWork
         $page = 1;
         do {
             $numInfo = $numberPlatform->getNumberCurl($page);
-            foreach ($numInfo['data']['numbers'] as $item) {
-                $phoneNumber = $item['numberName'];
-                if (
-                    strstr($phoneNumber, '111') ||
-                    strstr($phoneNumber, '222') ||
-                    strstr($phoneNumber, '333') ||
-                    strstr($phoneNumber, '444') ||
-                    strstr($phoneNumber, '555') ||
-                    strstr($phoneNumber, '666') ||
-                    strstr($phoneNumber, '777') ||
-                    strstr($phoneNumber, '888') ||
-                    strstr($phoneNumber, '999') ||
-                    strstr($phoneNumber, '000')) {
-                    $universityModel->type = "number";
-                    $universityModel->req_body = "豹子";
-                    $universityModel->content = $phoneNumber;
-                    $universityModel->created_at=time();
-                    $universityModel->save();
+            if (is_array($numInfo['data']['numbers'])) {
+                foreach ($numInfo['data']['numbers'] as $item) {
+                    $phoneNumber = $item['numberName'];
+                    if (
+                        strstr($phoneNumber, '111') ||
+                        strstr($phoneNumber, '222') ||
+                        strstr($phoneNumber, '333') ||
+                        strstr($phoneNumber, '444') ||
+                        strstr($phoneNumber, '555') ||
+                        strstr($phoneNumber, '666') ||
+                        strstr($phoneNumber, '777') ||
+                        strstr($phoneNumber, '888') ||
+                        strstr($phoneNumber, '999') ||
+                        strstr($phoneNumber, '000')) {
+                        $universityModel->type = "number";
+                        $universityModel->req_body = "豹子";
+                        $universityModel->content = $phoneNumber;
+                        $universityModel->created_at=time();
+                        $universityModel->save();
+                    }
+                    if (
+                        strstr($phoneNumber, '5220') ||
+                        strstr($phoneNumber, '5221') ||
+                        strstr($phoneNumber, '1996')
+                    ) {
+                        $universityModel->type = "number";
+                        $universityModel->req_body = "个人";
+                        $universityModel->content = $phoneNumber;
+                        $universityModel->created_at=time();
+                        $universityModel->save();
+                    }
+                    $page++;
                 }
-                if (
-                    strstr($phoneNumber, '5220') ||
-                    strstr($phoneNumber, '5221') ||
-                    strstr($phoneNumber, '1996')
-                ) {
-                    $universityModel->type = "number";
-                    $universityModel->req_body = "个人";
-                    $universityModel->content = $phoneNumber;
-                    $universityModel->created_at=time();
-                    $universityModel->save();
-                }
-                $page++;
             }
+
         } while (!empty($item['numberName']));
     }
 }
