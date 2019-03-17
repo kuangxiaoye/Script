728df8e010b769c4b57fecf9f16539b702726b9a
王野 <wangye@miot.cn>
2019-03-17 12:27:34 +0800

    gsx基本接口

--- app/Http/Controllers/GsxController.php
+++ app/Http/Controllers/GsxController.php
@@ -13,12 +13,38 @@ class GsxController extends BaseController
         $basesWork = new \App\Work\Gsx\Bases();
         $input = $request->input();
         if (empty($input)) {
-            $input = [
-                '123',
-            ];
+            return response()->json([
+                'msg'  => '你还什么都没填呢..',
+                'code' => 40001,
+            ]);
         }
-        $basesWork->getUserInfo($input);
+        $result = $basesWork->handleUserInfo($input);
 
-        return response()->json($input);
+        return response()->json($result);
+    }
+
+    public function searchschedule(Request $request)
+    {
+        $basesWork = new \App\Work\Gsx\Bases();
+        $email = $request->input('emial');
+
+        //初步邮箱验证
+        if (empty($email)) {
+            return response()->json([
+                'msg' => '请填写邮箱信息',
+            ]);
+        }
+
+        //入库查询是否存在该email
+        $emailSql = '';
+        if (empty($emailSql)) {
+            return response()->json([
+                'msg' => '该邮箱不存在',
+            ]);
+        }
+
+        $schedule = $basesWork->handleSchedule($emailSql);
+
+        return response()->json($schedule);
     }
 }
--- app/Work/Gsx/Bases.php
+++ app/Work/Gsx/Bases.php
@@ -13,9 +13,40 @@ use App\Work\BasesWork;
 
 class Bases extends BasesWork
 {
-    public function getUserInfo($input)
+    public function handleUserInfo($input)
     {
+        //将设备信息插入数据库 status0;
+        return [
+            'msg'  => '提交成功,请耐心等待.您也可以通过邮箱地址实时查询鉴定过程哦',
+            'code' => 200,
+        ];
+    }
 
-        return $input;
+    public function handleSchedule()
+    {
+        //在这里查库 如果status为1 ,那么就是已经鉴定完毕,如果是0.就返回鉴定中 ,后续可能存在更多状态码.
+        $status = 0;
+        switch ($status) {
+            case 0:
+                $return = [
+                    'msg'  => '努力查询中..嘿咻..',
+                    'code' => 40004,
+                ];
+                break;
+            case 1:
+                $return = [
+                    'msg'  => '查询成功啦,为了您的信息安全,请您登录邮箱查看Gsx鉴定结果呢!',
+                    'code' => 40004,
+                ];
+                break;
+            default:
+                $return = [
+                    'msg'  => '啊呀,进度失常了呢,好怕会不会翻车,请联系客服处理呢',
+                    'code' => 40004,
+                ];
+                break;
+        }
+
+        return $return;
     }
 }
